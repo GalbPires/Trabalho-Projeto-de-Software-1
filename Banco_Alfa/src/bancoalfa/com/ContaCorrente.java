86258f7e4cf316dc1fe603c2dbd9a12ff892ed92
GalbPires <37561564+GalbPires@users.noreply.github.com>
2018-04-16 21:35:36 -0300

    Exercício de simulação de conta bancária

---
+++ Banco_Alfa/src/bancoalfa/com/ContaCorrente.java
@@ -0,0 +1,35 @@
+package bancoalfa.com;
+
+public class ContaCorrente {
+
+	Cliente cliente = new Cliente();
+	String nomeDoCliente;
+	String numeroDaAgencia;
+	String numeroDaConta;
+	private double saldo = 1000.0;
+	double limite = 100.0;
+	boolean premiada = true;
+	
+	public boolean sacar(double valor) {
+		if(saldo + limite >= valor) {
+			saldo = saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void depositar(double valor) {
+		saldo = saldo + valor;
+	}
+	
+	public void transferirPara(ContaCorrente outraConta, 
+			double valor) {
+		sacar(valor);
+		outraConta.depositar(valor);
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+}
